888327c94629341b4392c9cd538693ec8ca0d1d2
Joseph Mead <mead@bnl.gov>
2025-04-16 08:23:47 -0400

    Add individual Fault Enable Bits

--- testing/ioc/st.cmd
+++ testing/ioc/st.cmd
@@ -66,7 +66,7 @@ dbLoadRecords("$(PSC_DBDIR)/wfmstats.db", "P=$(IOCNAME), PSC=1")
 
 
 #####################################################
-var(PSCDebug, 5)	#5 full debug
+var(PSCDebug, 1)	#5 full debug
 
 #psc1 Create the PSC
 createPSC("Tx1", $(PSC1_IP), 7, 0)
